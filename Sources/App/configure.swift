3ae416628bc7c9d07c8814c77819f1c5a976edde
moonjs0113 <littlemoom@naver.com>
2022-05-25 08:46:46 +0900

    Edit DB Into

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -10,11 +10,12 @@ public func configure(_ app: Application) throws {
     app.databases.use(.postgres(
         hostname: Environment.get("DATABASE_HOST") ?? "localhost",
         port: Environment.get("DATABASE_PORT").flatMap(Int.init(_:)) ?? PostgresConfiguration.ianaPortNumber,
-        username: Environment.get("DATABASE_USERNAME") ?? "vapor_username",
-        password: Environment.get("DATABASE_PASSWORD") ?? "vapor_password",
-        database: Environment.get("DATABASE_NAME") ?? "vapor_database"
+        username: Environment.get("DATABASE_USERNAME") ?? "ubuntu",
+        password: Environment.get("DATABASE_PASSWORD") ?? "",
+        database: Environment.get("DATABASE_NAME") ?? "vapordb"
     ), as: .psql)
-
+    
+    print(app.databases)
     app.migrations.add(CreateTodo())
     app.migrations.add(TaskMigration())
 
