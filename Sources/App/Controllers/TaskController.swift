7f9215715c6491aad506bf6f92fc9d17b1522cb6
moonjs0113 <littlemoom@naver.com>
2022-06-10 16:53:21 +0900

    Fix Task Delete Function

--- Sources/App/Controllers/TaskController.swift
+++ Sources/App/Controllers/TaskController.swift
@@ -35,10 +35,15 @@ struct TaskController: RouteCollection {
     // id로 데이터 조회, 없으면 unwrap이 호출 되며 .notFound 오류 반환
     // flatMap 콜백으로 EventLoopFuture 반환 -> 중첩을 피하기 위하
     func delete(req: Request) throws -> EventLoopFuture<HTTPStatus> {
-        guard let todoIDString = req.parameters.get("todoID"), let todoID = UUID(todoIDString) else {
-            throw Abort(.badRequest, reason: "Invalid parameter `todoID`")
+        guard let UUIDString = req.parameters.get("id") else {
+            throw Abort(.badRequest, reason: "Failed Get id Parameters")
         }
-        return Task.find(todoID, on: req.db)//req.parameters.get("id"), on: req.db)
+        
+        guard let uuid = UUID(UUIDString) else {
+            throw Abort(.badRequest, reason: "Invalid parameter")
+        }
+        
+        return Task.find(uuid, on: req.db)//req.parameters.get("id"), on: req.db)
             .unwrap(or: Abort(.notFound))
             .flatMap {
                 $0.delete(on: req.db)
