8d1fa888ae165519669cef914ca8c30355273745
moonjs0113 <littlemoom@naver.com>
2022-07-25 09:54:20 +0900

    {First End}

--- QuizFetcher/QuizFetcher/main.swift
+++ QuizFetcher/QuizFetcher/main.swift
@@ -52,66 +52,83 @@ func createQuiz(rowValue: [RowValue?]) -> Quiz {
                 example: rowValue[7]?.v ?? "")
 }
 
-Timer.scheduledTimer(withTimeInterval: 4, repeats: true) { _ in
-    Task(priority: .high) {
-        do {
-            // 1. 모든 문제를 가져온다.
-            var allQuiz = try await NetworkManager.shared.requestAllQuiz()
-             print("first All Quiz: \(allQuiz)")
-            // 2. 시트 문제를 가져온다.
-            let sheetQuiz = try await requestJSONDataFromSheets()
+var dateFormatter: DateFormatter {
+    let dateFormatter = DateFormatter()
+    dateFormatter.locale = Locale(identifier: "ko")
+    dateFormatter.timeZone = TimeZone(abbreviation: "KST")
+    dateFormatter.dateFormat = "yyyyMMdd"
+    return dateFormatter
+}
 
-            // 3. 시트 문제랑 비교해서
-            
-            print(allQuiz.count, sheetQuiz.count)
-            
-            if allQuiz.count < sheetQuiz.count {
-                for i in allQuiz.count...sheetQuiz.count-1 {
-                    // 3-1. 새로운 문제를 서버에 업로드한다.
-                    try await NetworkManager.shared.registerQuiz(quiz: sheetQuiz[i])
-                }
-                // 3-2. 모든 문제를 업데이트한다.
-                allQuiz = try await NetworkManager.shared.requestAllQuiz()
-            }
-            
-            print("4. 어제의 퀴즈를 가져온다.")
-            // 4. 어제의 퀴즈를 가져온다.
-            let yesterDayQuiz = try await NetworkManager.shared.requestTodayQuiz()
-            print(yesterDayQuiz)
-            
-            // MARK: - Current
-            
-            // 5. 어제 문제를 User history에 추가
+func quizFetch() async throws {
+    // 1. 모든 문제를 가져온다.
+    var allQuiz = try await NetworkManager.shared.requestAllQuiz()
+//     print("first All Quiz: \(allQuiz)")
+    // 2. 시트 문제를 가져온다.
+    let sheetQuiz = try await requestJSONDataFromSheets()
+
+    // 3. 시트 문제랑 비교해서
+    
+    print(allQuiz.count, sheetQuiz.count)
+    
+    if allQuiz.count < sheetQuiz.count {
+        for i in allQuiz.count...sheetQuiz.count-1 {
+            // 3-1. 새로운 문제를 서버에 업로드한다.
+            try await NetworkManager.shared.registerQuiz(quiz: sheetQuiz[i])
+        }
+        // 3-2. 모든 문제를 업데이트한다.
+        allQuiz = try await NetworkManager.shared.requestAllQuiz()
+    }
+    
+    // 4. 어제의 퀴즈를 가져온다.
+//    let yesterDayQuiz = try await NetworkManager.shared.requestTodayQuiz()
+//    print(yesterDayQuiz)
+    
+    // MARK: - Current
+    
+    // 5. 어제 문제를 User history에 추가
 //            for quiz in yesterDayQuiz {
 //                try await NetworkManager.shared.updateUserHistory(quiz: quiz)
 //            }
-            
-            // 6-1. 출제가 안된 문제들
-            
-            let notPublishedQuiz = allQuiz.filter { quiz in
-                print(quiz.isPublished)
-                return !quiz.isPublished
-            }
-            
-            print(notPublishedQuiz.count)
-            
-            // 6-2. 3문제 이상이 안 되면 에러
-            if notPublishedQuiz.count < 3 {
-                throw NetworkError.notEnoughQuiz
-            }
+    
+    // 6-1. 출제가 안된 문제들
+    
+    let notPublishedQuiz = allQuiz.filter { quiz in
+//        print(quiz.isPublished)
+        return !quiz.isPublished
+    }
+    
+//    print(notPublishedQuiz.count)
+    
+    // 6-2. 3문제 이상이 안 되면 에러
+    if notPublishedQuiz.count < 3 {
+        throw NetworkError.notEnoughQuiz
+    }
 
-            // 7. 기존 오늘의 문제 삭제
-            try await NetworkManager.shared.deleteTodayQuiz()
-            
-            for todayQuiz in notPublishedQuiz[0...2] {
+    // 7. 기존 오늘의 문제 삭제
+    try await NetworkManager.shared.deleteTodayQuiz()
+    
+    for todayQuiz in notPublishedQuiz[0...2] {
 //                 8. 오늘의 문제 등록
-                print(todayQuiz)
-                try await NetworkManager.shared.registerTodayQuiz(quiz: todayQuiz)
+//        print(todayQuiz)
+        try await NetworkManager.shared.registerTodayQuiz(quiz: todayQuiz)
 
 //                 9. Quiz 테이블에 isPublished 업데이트
-                try await NetworkManager.shared.updateTodayQuiz(quiz: todayQuiz)
+        try await NetworkManager.shared.updateTodayQuiz(quiz: todayQuiz)
+    }
+    print("Finish!")
+}
+
+var savedDataInt = 0
+
+Timer.scheduledTimer(withTimeInterval: 10, repeats: true) { _ in
+    Task(priority: .high) {
+        do {
+            let nowDateInt = Int(dateFormatter.string(from: Date())) ?? 0
+            if nowDateInt > savedDataInt {
+                try await quizFetch()
+                savedDataInt = Int(dateFormatter.string(from: Date())) ?? 0
             }
-            print("Finish!")
         } catch(let e as NetworkError) {
             print(e)
         }
